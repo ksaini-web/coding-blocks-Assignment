4d1043f085dc9bdfacebf5ff4c9421862a9da802
ksaini-web <kartiksaini9084@gmail.com>
2026-02-24 15:52:37 +0530

    Create PreviousSmallerElement.cpp

---
+++ PreviousSmallerElement.cpp
@@ -0,0 +1,45 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+vector <int> preSmallerElement(vector<int> arr){
+    
+    vector<int>ans(arr.size());
+    stack<int>s;
+    
+    for(int i =0 ; i < arr.size();i++){
+    
+    while(s.size()>0 && s.top() >= arr[i]){
+        s.pop();
+    }
+    
+    if(s.empty()){
+        ans[i]=-1;
+    }
+    else{
+      ans[i]=s.top();  
+    }
+                 
+      s.push(arr[i]);  
+        
+    }
+    return ans;
+}
+
+
+int main() {
+    vector<int> arr ={
+        3,1,0,8,6
+    };
+    
+    vector<int> ans = preSmallerElement(arr);
+    
+    for(int val : ans){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+  return 0;  
+    
+}
